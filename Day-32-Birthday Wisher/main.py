cb5f2b098c0eb5f5ca90f7b42eaea6d563475fdd
Ashton Jordan <ashton.jordan@gmail.com>
2023-11-21 16:54:47 -0600

    Created Tester app that sends random qoutes from a text file to a recipent email. User has to define sender address, sender email password, receiver email. Along with email smtp server details

--- Day-32-Birthday Wisher/main.py
+++ Day-32-Birthday Wisher/main.py
@@ -1,6 +1,29 @@
+import random
 import smtplib
+import datetime as dt
+from random import choice
 
-# Defined sending email
-my_email = "dev.ashton18@gmail.com"
+# Define sender, receiver, sender email password
+sender_email = "user@user.com"
+recipient_email = "user@user.com"
+password = "<sender email passsword>"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+# Initialize Datetime Object, and store current day integer in day_of_week variable
+now = dt.datetime.now()
+day_of_week = now.weekday()
+
+# Determine if the current day is Monday, if so send motivational quote to recipient email
+if day_of_week == 0:
+    # Open quotes.txt, and store all quotes in a list.
+    with open("quotes.txt", "r") as quotes:
+        q_list = quotes.readlines()
+        random_quote = random.choice(q_list)
+
+    # Send email with the qoute of the day to the "to_addrs" email
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=sender_email, password=password)
+        connection.sendmail(from_addr=sender_email,
+                            to_addrs=recipient_email,
+                            msg=f"Subject:Monday Motivation\n\n{random_quote}"
+                            )
