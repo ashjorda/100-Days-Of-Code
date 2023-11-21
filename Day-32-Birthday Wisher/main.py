73f89274cff5e3eefa2ce3564979b992c075aae4
Ashton Jordan <ashton.jordan@gmail.com>
2023-11-21 17:15:52 -0600

    Moved concept project code to Concept-Code folder, and added the core project files to the root directory of this folder.

--- Day-32-Birthday Wisher/main.py
+++ Day-32-Birthday Wisher/main.py
@@ -1,29 +1,13 @@
-import random
-import smtplib
-import datetime as dt
-from random import choice
+##################### Extra Hard Starting Project ######################
 
-# Define sender, receiver, sender email password
-sender_email = "user@user.com"
-recipient_email = "user@user.com"
-password = "<sender email passsword>"
+# 1. Update the birthdays.csv
 
-# Initialize Datetime Object, and store current day integer in day_of_week variable
-now = dt.datetime.now()
-day_of_week = now.weekday()
+# 2. Check if today matches a birthday in the birthdays.csv
 
-# Determine if the current day is Monday, if so send motivational quote to recipient email
-if day_of_week == 0:
-    # Open quotes.txt, and store all quotes in a list.
-    with open("quotes.txt", "r") as quotes:
-        q_list = quotes.readlines()
-        random_quote = random.choice(q_list)
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
-    # Send email with the qoute of the day to the "to_addrs" email
-    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
-        connection.starttls()
-        connection.login(user=sender_email, password=password)
-        connection.sendmail(from_addr=sender_email,
-                            to_addrs=recipient_email,
-                            msg=f"Subject:Monday Motivation\n\n{random_quote}"
-                            )
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
+
