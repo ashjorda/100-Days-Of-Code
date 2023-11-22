dfd80163e2cadacc2074287c5031212b70968cbb
Ashton Jordan <ashton.jordan@gmail.com>
2023-11-21 22:08:48 -0600

    Created mechanisms to read the birthdays into a pandas df, added variables to hold extracted name, email for each birthday recipient, mechanism to replace name place holder, and added SMTP code for email sending.

--- Day-32-Birthday Wisher/main.py
+++ Day-32-Birthday Wisher/main.py
@@ -1,13 +1,50 @@
 ##################### Extra Hard Starting Project ######################
-
 # 1. Update the birthdays.csv
-
 # 2. Check if today matches a birthday in the birthdays.csv
-
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
 # 4. Send the letter generated in step 3 to that person's email address.
 
+import smtplib
+import datetime as dt
+import pandas as pd
+import random
+
+# Program Variables
+sender_email = "user@user.com"
+recipient_email = "user@user.com"
+password = "sender email password"
+now = dt.datetime.now()
+day_of_month = now.day
+current_month = now.month
+
+# Birthday letter template file names from /letter_templates directory
+templates = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+
+# Open birthday recipients list
+df = pd.read_csv("birthdays.csv")
+# Stores the row index(s) in birthday_list where the current month/day matches a row within the birthdays.csv file
+birthday_list = df.loc[(df['month'] == current_month) & (df['day'] == day_of_month)].index
 
 
+for recipient in birthday_list:
+    # Extracts the name, email from the returned indexes within birthday_list dataframe
+    birthday_name = df.at[recipient, "name"]
+    birthday_email = df.at[recipient, "email"]
 
+    # Randomly chooses a letter_template from the template list, then reads the template into draft, and
+    # replaces the [NAME] placeholder with the birthday_name variable.
+    # Then store the template with the proper name in new_letter
+
+    birthday_template = random.choice(templates)
+    with open(f"letter_templates/{birthday_template}", "r") as draft_letter:
+        draft = draft_letter.read()
+        new_letter = draft.replace("[NAME]", birthday_name)
+
+        # Sends the new_letter to the recipient email
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=sender_email, password=password)
+            connection.sendmail(from_addr=sender_email,
+                                to_addrs=recipient_email,
+                                msg=f"Happy Birthday!\n\n{new_letter}"
+                                )
