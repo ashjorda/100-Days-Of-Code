b6edf2d5a8763314cead328a1caca4c6070b126a
Ashton Jordan <ashton.jordan@gmail.com>
2023-11-25 22:39:16 -0600

    added ISS main.py, and configured it for initial commit

---
+++ Day-33-ISS-Overhead-Notifier/main.py
@@ -0,0 +1,26 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 31.968599
+MY_LONG = -99.901810
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+
+print(time_now.hour)
