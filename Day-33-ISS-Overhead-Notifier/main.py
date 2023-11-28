ca905241c47b56b7068ba0ba31f6d6e00d0e4ac6
Ashton Jordan <ashton.jordan@gmail.com>
2023-11-28 15:14:07 -0600

    Added comments to the code for understanding and readability.

--- Day-33-ISS-Overhead-Notifier/main.py
+++ Day-33-ISS-Overhead-Notifier/main.py
@@ -3,6 +3,8 @@ import smtplib
 from datetime import datetime
 import time
 
+# Set the Latitude/Longitude coordinates for your location, and the sender email, sender email password, and recipient
+# email of the ISS overheard notification
 MY_LAT = 46.412830  # Your latitude
 MY_LONG = -1.409850  # Your longitude
 SENDER_EMAIL = "user@user.com"
@@ -10,7 +12,10 @@ SENDER_PASSWORD = "sending email password"
 RECIPIENT_EMAIL = "user@user.com"
 
 
-def up_above():
+# Function gets the long/lat of the ISS via API, and also raises an exception if the api call is not successful.
+# The API response is stored in data. Then parsed for the exact values needed. Then we compare if our long/lat is within
+# 5 degrees of the current ISS long/lat.Then return True if the if statemet checks out.
+def iss_overhead():
     response = requests.get(url="http://api.open-notify.org/iss-now.json")
     response.raise_for_status()
     data = response.json()
@@ -23,7 +28,10 @@ def up_above():
         return True
 
 
-def is_dark():
+# Function checks if it is nighttime at your long/lat location via API. It uses the API to get the sunset, and sunrise
+# time of your location. And check the current time to see if the current time is after sunset, and before sunrise.
+# If the condition is True, the function returns True.
+def dark_outside():
     parameters = {
         "lat": MY_LAT,
         "lng": MY_LONG,
@@ -36,17 +44,19 @@ def is_dark():
     sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
     sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-    time_now = datetime.now().hour
+    current_time = datetime.now().hour
 
-    if time_now >= sunset or time_now <= sunrise:
+    if current_time >= sunset or current_time <= sunrise:
         return True
 
 
+# While loop runs the code every 60 seconds, until stopped by user input or an exception is hit.
 while True:
     time.sleep(60)
-    if up_above() and is_dark():
-        # Sends the new_letter to the recipient email using gmail smtp. Can replace smtp with your email provider server
-        # and secure port
+    # If ISS is close, and it's nighttime. Send an email notifying ISS is close.
+    if iss_overhead() and dark_outside():
+        # Sends an email to the recipient email using the set smtp. Stating that the ISS is within 5+ or 5- degrees of
+        # MY_LAT, MY_LONG above.
         with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
             connection.starttls()
             connection.login(user=SENDER_EMAIL, password=SENDER_PASSWORD)
