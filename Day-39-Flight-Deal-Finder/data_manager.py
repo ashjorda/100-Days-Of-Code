f2fa2a6c35f025f996d7fca832f3c80de594779e
Ashton Jordan <ashton.jordan@gmail.com>
2023-12-13 21:58:14 -0600

    Completed the city code population in google sheets, and refactored main.py, and updated flight_search.py.

--- Day-39-Flight-Deal-Finder/data_manager.py
+++ Day-39-Flight-Deal-Finder/data_manager.py
@@ -4,24 +4,19 @@ from flight_search import FlightSearch
 
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
-    def get_sheetly_data():
+    def import_data(self):
         api_url = 'https://api.sheety.co/c0f6856a82dab9a3b8def4c44041939b/flightDeals/prices'
         data = requests.get(url=api_url)
-        return data.json()['prices']
+        return data.json()
 
-    def check_iata(sheetdata):
-        for _ in sheetdata:
+    def city_code_exist(self, data):
+        for _ in data['prices']:
             if len(_['iataCode']) == 0:
                 body = {
                     "price": {
-                        "iataCode": FlightSearch.get_iata(_['city']),
+                        "iataCode": FlightSearch.city_code(_['city']),
                     }
                 }
                 requests.put(url=f"https://api.sheety.co/c0f6856a82dab9a3b8def4c44041939b/flightDeals/prices/{_['id']}",
                              json=body)
 
-    def get_iata():
-        api_url = 'https://api.sheety.co/c0f6856a82dab9a3b8def4c44041939b/flightDeals/prices'
-        data = requests.get(url=api_url)
-        for iata in data.json()['prices']:
-            print(FlightSearch.flight_price(iata['iataCode']))
--- Day-39-Flight-Deal-Finder/main.py
+++ Day-39-Flight-Deal-Finder/main.py
@@ -1,13 +1,23 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
 # program requirements.
-#
+
+import flight_search
+from datetime import datetime, timedelta
 from data_manager import DataManager
-from flight_search import FlightSearch
 
-flight_search = FlightSearch
-data_manager = DataManager
+flight_data = DataManager()
 
-# # data_manager.check_iata(data_manager.get_sheetly_data())
+# Import Flight Data from Sheetly. Check if Data contains city codes for each destination, if not populate them
+#destination_list = flight_data.import_data()
+destination_list = {'prices': [{'city': 'Paris', 'iataCode': 'PAR', 'lowestPrice': 54, 'id': 2}, {'city': 'Berlin', 'iataCode': 'BER', 'lowestPrice': 42, 'id': 3}, {'city': 'Tokyo', 'iataCode': 'TYO', 'lowestPrice': 485, 'id': 4}, {'city': 'Sydney', 'iataCode': 'SYD', 'lowestPrice': 551, 'id': 5}, {'city': 'Istanbul', 'iataCode': 'IST', 'lowestPrice': 95, 'id': 6}, {'city': 'Kuala Lumpur', 'iataCode': 'KUL', 'lowestPrice': 414, 'id': 7}, {'city': 'New York', 'iataCode': 'NYC', 'lowestPrice': 240, 'id': 8}, {'city': 'San Francisco', 'iataCode': 'SFO', 'lowestPrice': 260, 'id': 9}, {'city': 'Cape Town', 'iataCode': 'CPT', 'lowestPrice': 378, 'id': 10}]}
+flight_data.city_code_exist(destination_list)
 
 
-DataManager.get_iata()
+# Check flights
+ORIGIN_CITY_IATA = "DFW"
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_future = tomorrow + timedelta(days=180)
+
+for city in destination_list['prices']:
+    dest_city = city['iataCode']
+    flight_search.flight_price(ORIGIN_CITY_IATA, dest_city, tomorrow, six_months_future)
