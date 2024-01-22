c0387807b9633ff9bfd54d1857e62c59116e0f13
Ashton Jordan <ashton.jordan@gmail.com>
2024-01-22 16:53:24 -0600

    Added more examples of how to use bs.

--- Day-45-Web-Scraping/main.py
+++ Day-45-Web-Scraping/main.py
@@ -13,7 +13,7 @@ soup = BeautifulSoup(contents, "html.parser")
 # print(soup.p)
 
 # Finds all the tags by name, and stores thim in a list in the variable
-all_anchor_tags = soup.find_all(name="a")
+# all_anchor_tags = soup.find_all(name="a")
 # print(all_anchor_tags)
 
 # loops through the all_anchor_tag variable, and uses the getText() module to grab the text
@@ -25,5 +25,12 @@ all_anchor_tags = soup.find_all(name="a")
 #     print(tag.get("href"))
 
 # Finds an element, and allows for the specification of the id attribute
-heading = soup.find(name="h1", id="name")
-print(heading)
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# Allows you to select a class, as class is a python reserved name. so you use class_ to grab class objects
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading)
+
+company_url = soup.select_one(selector="p a")
+print(company_url)
