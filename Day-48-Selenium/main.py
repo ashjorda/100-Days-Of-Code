0623b5db90141b3069f4f6ce16e9b2481f18fb59
Ashton Jordan <ashjorda@cisco.com>
2024-03-02 22:05:05 -0600

    Fixed merg conflicts

--- Day-48-Selenium/main.py
+++ Day-48-Selenium/main.py
@@ -6,30 +6,12 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-<<<<<<< HEAD
-driver.get("https://python.org/")
-
-# price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-# price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-# print(f"The price is {price_dollar.text}.{price_cents.text}")
-
-# search_bar = driver.find_element(By.NAME, value="q")
-# print(search_bar.get_attribute("placeholder"))
-# button = driver.find_element(By.ID, value="submit")
-# print(button.size)
-# documentation_link = driver.find_element(By.CSS_SELECTOR, value=".documentation-widget a")
-# print(documentation_link.text)
-
-bug_link = driver.find_element(By.XPATH, value='//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
-print(bug_link.text)
-=======
 driver.get("https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/ref=sr_1_3?crid"
            "=2554HN0H9U8C6&keywords=instant%2Bpot&qid=1708032382&sprefix=insta%2Caps%2C129&sr=8-3&th=1")
 
 price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
 price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
 print(f"The price is {price_dollar.text}.{price_cents.text}")
->>>>>>> de12005 (Cleaned up the directory structure)
 
 # driver.close()
 driver.quit()
