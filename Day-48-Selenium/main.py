a5a763e6aeeae97e6e86cc784c454c023d8af04e
Ashton Jordan <ashjorda@cisco.com>
2024-03-08 21:43:42 -0600

    Added code to create a nested dictionary of the date, name key/vaule pair for each event

--- Day-48-Selenium/main.py
+++ Day-48-Selenium/main.py
@@ -11,10 +11,15 @@ driver.get("https://python.org")
 
 element = driver.find_element(By.TAG_NAME, 'div')
 e_class = element.find_element(By.CLASS_NAME, 'event-widget')
-events = e_class.find_elements(By.TAG_NAME, 'li')
+event_list = e_class.find_elements(By.TAG_NAME, 'li')
 
-for date in events:
-    print(date.text)
+event_data = {}
 
+key = 0
+for event in event_list:
+    event_data[key] = {'time': event.text.split('\n')[0], 'name': event.text.split('\n')[1]}
+    key +=1
+
+print(event_data)
 # driver.close()
 driver.quit()
