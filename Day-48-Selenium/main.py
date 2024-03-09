73cf2f4f2b4cccab5ba4209c9b092fc27d09f23a
Ashton Jordan <ashjorda@cisco.com>
2024-03-08 21:14:18 -0600

    Created code to extract current events hosted by python.org

--- Day-48-Selenium/main.py
+++ Day-48-Selenium/main.py
@@ -6,12 +6,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com/Instant-Pot-Multi-Use-Programmable-Pressure/dp/B00FLYWNYQ/ref=sr_1_3?crid"
-           "=2554HN0H9U8C6&keywords=instant%2Bpot&qid=1708032382&sprefix=insta%2Caps%2C129&sr=8-3&th=1")
-
-price_dollar = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-price_cents = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-print(f"The price is {price_dollar.text}.{price_cents.text}")
+driver.get("https://www.amazon.com")
 
 # driver.close()
 driver.quit()
