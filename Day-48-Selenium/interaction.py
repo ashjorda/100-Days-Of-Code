9f435d4b806bea10b157a5862c7694aac83e4e5d
Ashton Jordan <ashjorda@cisco.com>
2024-03-10 14:50:48 -0500

    Commented out the click action for the article count link

--- Day-48-Selenium/interaction.py
+++ Day-48-Selenium/interaction.py
@@ -8,8 +8,9 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
+# article_count = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
+# article_count.click()
 
-print(article_count.text)
+# print(article_count.text)
     
 driver.quit()
