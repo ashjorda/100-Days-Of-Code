74b99cd13da0e181b33af86e9abf5f8a7e3b0198
Ashton Jordan <ashjorda@cisco.com>
2024-03-10 15:19:14 -0500

    Added code comments

--- Day-48-Selenium/challenge.py
+++ Day-48-Selenium/challenge.py
@@ -8,17 +8,20 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 
 # Signup for the LAB report
-
 driver.get("https://secure-retreat-92358.herokuapp.com/")
 
+# Enter First Name on the form
 fname = driver.find_element(By.NAME, value="fName")
 fname.send_keys("John")
 
+# Enter Last Name in the form
 lname = driver.find_element(By.NAME, value="lName")
 lname.send_keys("Dough")
 
+# Enter email address in the form
 email = driver.find_element(By.NAME, value="email")
 email.send_keys("jdough@gmail.com")
 
+# Click the submit button
 submit_button = driver.find_element(By.CSS_SELECTOR, value='.btn')
 submit_button.click()
