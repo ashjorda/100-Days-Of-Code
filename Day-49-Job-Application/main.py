4d9a3eec2bc74819b1158b83aafc2ba6acbbcacc
Ashton Jordan <ashjorda@cisco.com>
2024-03-19 17:01:57 -0500

    Created the click action for the sigin-in button in Linkedin

--- Day-49-Job-Application/main.py
+++ Day-49-Job-Application/main.py
@@ -9,3 +9,9 @@ driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
 
+# Sigin into Linkedin
+sign_in = driver.find_element(By.CLASS_NAME, value="btn-secondary-emphasis")
+sign_in.click()
+
+
+
