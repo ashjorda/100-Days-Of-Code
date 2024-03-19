a817b34cf052c7f752bb8513b7f25425fdc72181
Ashton Jordan <ashjorda@cisco.com>
2024-03-19 16:41:30 -0500

    Add boiler plate chromium settings, and the linkedin url for the job posting I will use to apply to.

---
+++ Day-49-Job-Application/main.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Keep Chrome browser open after program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+
+driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
+
