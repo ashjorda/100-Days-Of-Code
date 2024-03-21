caa443dc514013d2bdf08f4920b93fd05ad83725
Ashton Jordan <ashjorda@cisco.com>
2024-03-21 16:43:39 -0500

    "Added the By.LINK_TEXT driver find element for sigin in button. And added a few more sleep timers. Alogn with adding a driver for clicking save on the Job listings."

--- Day-49-Job-Application/main.py
+++ Day-49-Job-Application/main.py
@@ -11,7 +11,8 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=102257491&keywords=python%20developer&location=London%2C%20England%2C%20United%20Kingdom&redirect=false&position=1&pageNum=0")
 
 # Sigin into Linkedin
-sign_in = driver.find_element(By.CLASS_NAME, value="btn-secondary-emphasis")
+# sign_in = driver.find_element(By.CLASS_NAME, value="btn-secondary-emphasis")
+sign_in = driver.find_element(By.LINK_TEXT, value="Sign in")
 sign_in.click()
 
 # Wait 5 seconds before entering the login credentials
@@ -25,5 +26,13 @@ username.send_keys("dev.linkedin6@gmail.com")
 password = driver.find_element(By.ID, value="password")
 password.send_keys("BE.mature.2018")
 
+time.sleep(3) # Wait 3 seconds before clicking the sign in button
+
+# Click the sign in button
 click_sign_in = driver.find_element(By.CLASS_NAME, value="btn__primary--large ")
 click_sign_in.click()
+
+time.sleep(3) # Wait 3 seconds before clicking the sign in button
+# Click the save button
+click_save = driver.find_element(By.CLASS_NAME, value="jobs-save-button")
+click_save.click()
