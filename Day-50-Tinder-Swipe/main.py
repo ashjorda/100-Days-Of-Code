0fcec9f9f76e2138cd2cf2f24f7c91e429ae4beb
Ashton Jordan <ashjorda@cisco.com>
2024-03-29 14:58:15 -0500

    Removed unused commented out code

--- Day-50-Tinder-Swipe/main.py
+++ Day-50-Tinder-Swipe/main.py
@@ -23,26 +23,3 @@ time.sleep(5)
 # Switch frame by id
 login_google = driver.find_element(By.XPATH, '//*[@id="gsi_802342_709961"]')
 login_google.click()
-
-
-
-# # # Enter Username
-# username = driver.find_element(By.ID, value="username")
-# username.send_keys("dev.linkedin6@gmail.com")
-
-# # Enter Password
-# password = driver.find_element(By.ID, value="password")
-# password.send_keys("BE.mature.2018")
-
-# time.sleep(3) # Wait 3 seconds before clicking the sign in button
-
-# # Click the sign in button
-# click_sign_in = driver.find_element(By.CLASS_NAME, value="btn__primary--large ")
-# click_sign_in.click()
-
-# time.sleep(3) # Wait 3 seconds before clicking the sign in button
-# # Click the save button
-# click_save = driver.find_element(By.CLASS_NAME, value="jobs-save-button")
-# click_save.click()
-
-#Complete
