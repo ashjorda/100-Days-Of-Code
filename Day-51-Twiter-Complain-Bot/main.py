7a35fb7b01d1d6b07c579ea4a197affef85d604a
Ashton Jordan <ashjorda@cisco.com>
2024-04-11 16:29:29 -0500

    Corrected the CHROME_DRIVER_PATH constnat

--- Day-51-Twiter-Complain-Bot/main.py
+++ Day-51-Twiter-Complain-Bot/main.py
@@ -1,5 +1,5 @@
 PROMISED_DOWN = 150
 PROMISED_UP = 10
-CHROME_DRIVER_PATH = "C:/Development/chromedriver.exe"
+CHROME_DRIVER_PATH = "/Users/ashjorda/Development/chromedriver"
 TWITTER_EMAIL = "ashton.jordan@gmail.com"
 TWITTER_PASSWORD = "YOUR PASSWORD"
