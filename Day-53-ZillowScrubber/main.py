1af066ea40ee861b6a1f595848f763a71ed716ca
Ashton Jordan <ashjorda@cisco.com>
2024-05-01 14:46:16 -0500

    Added property price logic, to store scrubbed property prices into a list.

--- Day-53-ZillowScrubber/main.py
+++ Day-53-ZillowScrubber/main.py
@@ -7,3 +7,11 @@ listing_site_parsed = BeautifulSoup(listing_site.text, features="html.parser")
 
 property_address_links = [address['href'] for address in listing_site_parsed.find_all(name='a', href=True, class_="property-card-link")]
 
+property_prices = []
+for price in listing_site_parsed.find_all(name='span', class_="PropertyCardWrapper__StyledPriceLine"):
+    price_slash = price.get_text().split('/')
+    price_plus = price_slash[0].split('+')
+    property_prices.append(price_plus[0])
+
+print(property_prices)
+form = "https://docs.google.com/forms/d/e/1FAIpQLScqwyl9a4WW8jBrjc0-Qu5Z77cOjMtkcsyvoBOr2IY76yfoFg/viewform?usp=sf_link"
