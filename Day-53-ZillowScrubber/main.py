4e2356f842ae5925cf6b35ec3fc5dda0b0972038
Ashton Jordan <ashjorda@cisco.com>
2024-05-01 14:51:14 -0500

    Added property address logic to parse property addresses

--- Day-53-ZillowScrubber/main.py
+++ Day-53-ZillowScrubber/main.py
@@ -5,7 +5,7 @@ listing_site = requests.get(url='https://appbrewery.github.io/Zillow-Clone/')
 
 listing_site_parsed = BeautifulSoup(listing_site.text, features="html.parser")
 
-property_address_links = [address['href'] for address in listing_site_parsed.find_all(name='a', href=True, class_="property-card-link")]
+property_listing = [listing_link['href'] for listing_link in listing_site_parsed.find_all(name='a', href=True, class_="property-card-link")]
 
 property_prices = []
 for price in listing_site_parsed.find_all(name='span', class_="PropertyCardWrapper__StyledPriceLine"):
@@ -13,5 +13,8 @@ for price in listing_site_parsed.find_all(name='span', class_="PropertyCardWrapp
     price_plus = price_slash[0].split('+')
     property_prices.append(price_plus[0])
 
-print(property_prices)
+property_address = [address.get_text(strip=True) for address in listing_site_parsed.find_all(name='address')]
+print(property_address)
+
+
 form = "https://docs.google.com/forms/d/e/1FAIpQLScqwyl9a4WW8jBrjc0-Qu5Z77cOjMtkcsyvoBOr2IY76yfoFg/viewform?usp=sf_link"
