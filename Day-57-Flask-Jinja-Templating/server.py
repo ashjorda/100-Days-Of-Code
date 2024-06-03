171a1ecd3fc534a70d5b2053ef20b29d5cb72390
Ashton Jordan <ashjorda@cisco.com>
2024-06-03 15:53:42 -0500

    feat: create /blog app route, with the defined api call to npoint.io. And create blog.html template for rendering of the json response using jinja2 using a for loop to iterate over all the blogs in the json response

--- Day-57-Flask-Jinja-Templating/server.py
+++ Day-57-Flask-Jinja-Templating/server.py
@@ -45,5 +45,13 @@ def guess(name):
     return render_template("guess.html", user=upper_name, age=guessed_age, gender=guessed_gender)
 
 
+@app.route('/blog')
+def blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=8001)
