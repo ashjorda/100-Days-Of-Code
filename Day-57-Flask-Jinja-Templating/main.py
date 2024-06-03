c04e5eab5265d362d60d4fa3b02909c076b5fa61
Ashton Jordan <ashjorda@cisco.com>
2024-06-03 16:44:26 -0500

    refac: with the "home" function, retrieve blogpost from nopint.io, and implement error handling. pass along the json response in a variable called blog to index.html

--- Day-57-Flask-Jinja-Templating/main.py
+++ Day-57-Flask-Jinja-Templating/main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    if response.status_code == 200:
+        blog_post = response.json()
+        return render_template("index.html", blogs=blog_post)
+    else:
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
