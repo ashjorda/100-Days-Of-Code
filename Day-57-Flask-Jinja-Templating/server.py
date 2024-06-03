f5ecc28c51439b2e1e4bae4cc5cef87115b25ff2
Ashton Jordan <ashjorda@cisco.com>
2024-06-03 14:40:00 -0500

    feat: create a footer that dynamically updates the year. Change default flask port to 8001

--- Day-57-Flask-Jinja-Templating/server.py
+++ Day-57-Flask-Jinja-Templating/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import datetime
 
 app = Flask(__name__)
 
@@ -7,8 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     random_number = random.randint(1, 10)
-    return render_template("index.html", num=random_number)
+    copyright_year = datetime.datetime.now().year
+    return render_template("index.html", num=random_number, year=copyright_year)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8001)
