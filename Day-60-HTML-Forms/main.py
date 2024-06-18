185f4e36ba0fcdcaca220c0a8493f36f0c36c878
Ashton Jordan <ashjorda@cisco.com>
2024-06-17 23:20:09 -0500

    chore: updated dynamic url for '/login' approute, and updated return value

--- Day-60-HTML-Forms/main.py
+++ Day-60-HTML-Forms/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,10 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def receive_data():
-    return "💪 Success! Form submitted"
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        return f"<h1>Name: {username}, Password: {password}</h1>"
 
 
 if __name__ == "__main__":
