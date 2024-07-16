ceed9e2a9c351ec31749e825ab046078873a858d
Ashton Jordan <ashjorda@cisco.com>
2024-07-16 16:05:12 -0500

    feature: Replace all_books list with a Sqlite Database named BookShelf

--- Day-63-Virtual Bookshelf/main.py
+++ Day-63-Virtual Bookshelf/main.py
@@ -1,9 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
 all_books = []
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///library-collection.db"
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+
+# initialize the app with the extension
+db.init_app(app)
+
+
+# CREATE TABLE
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    review: Mapped[float] = mapped_column(Float, nullable=False)
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
