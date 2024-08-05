c0ddf19dd210735cd59415fbccb6ef278153965a
Ashton Jordan <ashjorda@cisco.com>
2024-08-05 16:38:29 -0500

    refactor: Added /post to the approute, and declared the data type for security

--- Day-67-Upgraded-Blog/main.py
+++ Day-67-Upgraded-Blog/main.py
@@ -64,7 +64,7 @@ def get_all_posts():
 
 
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/<post_id>')
+@app.route('/post/<int:post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
     requested_post = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id)).scalar()
