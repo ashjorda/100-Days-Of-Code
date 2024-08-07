48cd8b85ad99d8b42f02480de42fa881faa1df74
Ashton Jordan <ashjorda@cisco.com>
2024-08-07 13:16:20 -0500

    feature: Added new route that renders make-post.html when the "New Post" button is clicked on index.html.

--- Day-67-Upgraded-Blog/main.py
+++ Day-67-Upgraded-Blog/main.py
@@ -72,6 +72,9 @@ def show_post(post_id):
 
 
 # TODO: add_new_post() to create a new blog post
+@app.route('/new-post')
+def new_post():
+    return render_template("make-post.html")
 
 # TODO: edit_post() to change an existing blog post
 
